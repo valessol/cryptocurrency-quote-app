f2c8fac16871ccc6c7ca2e6f9603d88e61528ec1
Valeria Silveira <ssilveira@nosis.com>
2023-02-24 14:41:53 -0300

    rename component

--- src/components/SelectCurrencies/SelectCurrencies.jsx
+++ src/components/SelectCurrencies/SelectCurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Label, Select } from "./styles";
 
 const SelectCurrencies = ({ options, onChange }) => {
