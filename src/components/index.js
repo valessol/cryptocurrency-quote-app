d764cb8882861cbe5e3e2def2c12230014b1abe6
Valeria Silveira <ssilveira@nosis.com>
2023-02-24 14:42:14 -0300

    add components to index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import CriptoData from "./CriptoData/CriptoData";
 import Error from "./Error/Error";
 import Form from "./Form/Form";
-export { Form, Error, CriptoData };
+import SelectCurrencies from "./SelectCurrencies/SelectCurrencies";
+import Spinner from "./Spinner/Spinner";
+export { Form, Error, CriptoData, SelectCurrencies, Spinner };
